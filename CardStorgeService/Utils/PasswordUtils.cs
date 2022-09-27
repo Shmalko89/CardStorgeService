2edec627d797342baeca47c92838ca76c12f26f8
Vladimir <shmalko89@list.ru>
2022-09-27 21:09:09 +0400

    Создание таблиц Аккаунтов и Сессий аккаунтов, миграция

---
+++ CardStorgeService/Utils/PasswordUtils.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CardStorgeService.Utils
+{
+    public class PasswordUtils
+    {
+        private const string SecretKey = "vmknrAS23523;[wefs";
+
+        public static (string passwordSalt, string passwordHash) CreatePasswordHash(string password)
+        {
+            byte[] buffer = new byte[16];
+            RNGCryptoServiceProvider secureRandom = new RNGCryptoServiceProvider();
+            secureRandom.GetBytes(buffer);
+
+            string passwordSalt = Convert.ToBase64String(buffer);
+            string passwordHash = GetPasswordHash(password, passwordSalt);
+            return (passwordHash, passwordSalt);
+        }
+
+        public static bool VerifyPassword(string password, string passwordSalt, string passwordHash)
+        {
+            return GetPasswordHash(password, passwordSalt) == passwordHash;
+        }
+
+        public static string GetPasswordHash(string password, string passwordSalt)
+        {
+            password = $"{password}~{passwordSalt}~{SecretKey}";
+            byte[] buffer = Encoding.UTF8.GetBytes(password);
+
+            SHA512 sha512 = new SHA512Managed();
+            byte[] passwordHash = sha512.ComputeHash(buffer);
+            return Convert.ToBase64String(passwordHash);
+        }
+
+    }
+}
